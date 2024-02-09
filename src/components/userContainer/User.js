4697b65ce7d4e51e0be1bb7cb48a026a64eb1fd6
rostikfebruary <lyutyirostik@gmail.com>
2024-02-09 12:10:46 +0200

    hw3 t1

--- src/components/userContainer/User.js
+++ src/components/userContainer/User.js
@@ -2,7 +2,8 @@ const User = ({post}) => {
     const { name, username, email} = post
     return (
         <div className={'user-block'}>
-            {/*<div>id: {id}</div>*/}
+            <div>id: {id}</div>
+            <div>id: {id}</div>
             <div>name: {name}</div>
             <div>username: {username}</div>
             <div className={'email'}>email: {email}</div>
